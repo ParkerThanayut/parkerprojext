f82dcaf484d69e188bab53d766c8b348bbd97284
ParkerThanayut <114637634+ParkerThanayut@users.noreply.github.com>
2025-05-11 22:48:04 +0700

    Delete api directory

--- api/webhook.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    // handle POST request
-    res.status(200).send('OK');
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
