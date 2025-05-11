fa80933bc40d0ca16c45c4b876a779cedddb21b3
ParkerThanayut <114637634+ParkerThanayut@users.noreply.github.com>
2025-05-11 14:28:22 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,20 +1,8 @@
-{
-  "name": "parkerprojext",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "type": "module",  <-- เพิ่มบรรทัดนี้
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "start": "next start"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "next": "^14.x",
-    "react": "^18.x",
-    "react-dom": "^18.x"
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    // handle POST request
+    res.status(200).send('OK');
+  } else {
+    res.status(405).send('Method Not Allowed');
   }
 }
